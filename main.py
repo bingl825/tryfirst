ef39e574d2d3991b03d3e7fd3bf75e3edf85a217
Rico <bingl_825@hotmail.com>
2020-02-03 11:49:48 +0800

    20200203 进度

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, request,render_template
 from config import DevConfig
 from flask_sqlalchemy import SQLAlchemy
 # import db_mod
@@ -13,33 +13,60 @@ class User(db.Model):
     id = db.Column(db.Integer(),primary_key = True)
     username = db.Column(db.String(20))
     password = db.Column(db.String(20))
-    diarys = db.relationship('Diary',backref = 'user',lazy = 'dynamic')
+
+class ProjUser(db.Model):
+    __tablename__ = 'ProjUser'
+    id = db.Column(db.Integer(), primary_key=True)
+    project = db.Column(db.Integer(),db.ForeignKey('Project.id'))
+    user = db.Column(db.Integer(),db.ForeignKey('User.id'))
+
+class Project(db.Model):
+    __tablename__ = 'Project'
+    id = db.Column(db.Integer(),primary_key = True)
+    name = db.Column(db.String(50))
+    permitTime = db.Column(db.Date())
+    startTime = db.Column(db.Date())
+    jianshe = db.Column(db.String(50))
+    shigong = db.Column(db.String(50))
+    sheji = db.Column(db.String(50))
+    kancha = db.Column(db.String(50))
+    jianli = db.Column(db.String(50),default = "城开监理")
+    zaojia = db.Column(db.Float(50))
+
+    diarys = db.relationship('Diary',backref = 'Project',lazy = 'dynamic')
+
+
 
 class Diary(db.Model):
     __tablename__ = 'Diary'
     id = db.Column(db.Integer(), primary_key=True)
     date = db.Column(db.Date())
     content = db.Column(db.String(255))
-    userid = db.Column(db.Integer(),db.ForeignKey('User.id'))
+    projid = db.Column(db.Integer(),db.ForeignKey('Project.id'))
 
-class Project(db.Model):
-    __tablename__ = 'Project'
-    id = db.Column(db.Integer(), primary_key=True)
-    content = db.Column(db.String(255))
-    content2 = db.Column(db.String(255))
-    content3 = db.Column(db.String(255))
-    content4 = db.Column(db.String(255))
-    userid = db.Column(db.Integer(),db.ForeignKey('User.id'))
+
+
 
 def list_name():
     res = User.query.all()
     list_name = res
     return list_name
 
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def home():
-    res = list_name()
-    return render_template('home.html',list_name = res)
+    res=Diary.query.filter(Diary.projid == 1).first()
+    return render_template('home.html',res=res.content)
+
+@app.route('/', methods = ['POST'])
+def home_in():
+    username = request.form['username']
+    password = request.form['password']
+  # 这里有一个空值判断strip()
+    if username.strip():
+        check = User.query.filter(User.username == username).first()
+        return render_template('home.html',check = check.username)
+    else:
+         return render_template('home.html',msg = "请输入名字")
 
 if __name__ == '__main__':
     app.run()
--- manage.py
+++ manage.py
@@ -1,18 +1,18 @@
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
-from main import app,db,User,Diary,Project
+from main import app,db,User,Diary,Project,ProjUser
 
 
 
 manager = Manager(app)
 migrate = Migrate(app,db)
 
-manager.add_command("server",Server())
+manager.add_command("server",Server('0.0.0.0'))
 manager.add_command("db",MigrateCommand)
 
 @manager.shell
 def make_shell_contet():
-    return dict(app = app, db = db, User = User, Diary = Diary, Project = Project)
+    return dict(app = app, db = db, User = User, Diary = Diary, Project = Project,ProUser = ProjUser)
 
 if __name__ == "__main__":
     manager.run()
